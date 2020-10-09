7947266da4f11cd82cc33c3468ce8c7924342c52
Michael Leal <64756971+OxenCarbon@users.noreply.github.com>
2020-10-08 19:04:40 -0500

    Ice Cream Gui
    un-edited
    
    Co-Authored-By: SergioLopez80745 <70538499+SergioLopez80745@users.noreply.github.com>

---
+++ IceCreamGUI/IceCreamGUI/EmployeeOptionTab.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace IceCreamGUI
+{
+    partial class EmployeeOptionTab
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnStartOrder = new System.Windows.Forms.Button();
+            this.btnSchedule = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Empolyee #";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // BtnStartOrder
+            // 
+            this.BtnStartOrder.Location = new System.Drawing.Point(84, 63);
+            this.BtnStartOrder.Name = "BtnStartOrder";
+            this.BtnStartOrder.Size = new System.Drawing.Size(146, 42);
+            this.BtnStartOrder.TabIndex = 2;
+            this.BtnStartOrder.Text = "Start Order";
+            this.BtnStartOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnSchedule
+            // 
+            this.btnSchedule.Location = new System.Drawing.Point(288, 63);
+            this.btnSchedule.Name = "btnSchedule";
+            this.btnSchedule.Size = new System.Drawing.Size(146, 42);
+            this.btnSchedule.TabIndex = 3;
+            this.btnSchedule.Text = "View Schedule";
+            this.btnSchedule.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 136);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(484, 112);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // EmployeeOptionTab
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(508, 260);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSchedule);
+            this.Controls.Add(this.BtnStartOrder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "EmployeeOptionTab";
+            this.Text = "EmployeeOptionTab";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnStartOrder;
+        private System.Windows.Forms.Button btnSchedule;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
